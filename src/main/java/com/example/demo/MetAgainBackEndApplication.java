2256a6b1394fe809d8f92ee84f15ef21dc8ead37
Grischakrombacha <grischa.storch@gmail.com>
2023-05-15 12:16:09 +0200

    initial commit

--- src/main/java/com/example/demo/MetAgainBackEndApplication.java
+++ src/main/java/com/example/demo/MetAgainBackEndApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MetAgainBackEndApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MetAgainBackEndApplication.class, args);
 	}
 
 }
